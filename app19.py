b3f7c54902a2fba64887e40c365fe0f54f918e0e
mailteam8 <teamsprojects8@gmail.com>
2026-03-28 02:34:17 +0300

    Remove download_model endpoint from app19.py
    Removed the download_model endpoint and its associated response.

--- app19.py
+++ app19.py
@@ -3,7 +3,6 @@ import pickle
 import base64
 import requests
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
 from supabase import create_client, Client
 
 app = FastAPI()
@@ -174,16 +173,3 @@ def predict_patient(pat_id: str):
         "count_readings": len(predictions),
         "predictions": predictions
     }
-
-# ✅ تنزيل النموذج مباشرة من الخدمة
-@app.get("/download/{pat_id}")
-def download_model(pat_id: str):
-    filename = f"heart_guard_{pat_id}.pkl"
-    if os.path.exists(filename):
-        return FileResponse(
-            filename,
-            media_type="application/octet-stream",
-            filename=filename
-        )
-    else:
-        return {"error": "⚠️ الملف غير موجود، درّب أولاً عبر /check أو /train"}
